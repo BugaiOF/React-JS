b8b7937aa583ac1aebfdc32e214187b6d98aa1dc
Gustavo Da Silva Silveira <112825271+BugaiOF@users.noreply.github.com>
2023-11-25 02:36:03 -0300

    Type Script

---
+++ Fundamentos-React-TS/Fundamentos-reactjs-ts/src/components/Post.tsx
@@ -0,0 +1,124 @@
+import { format, formatDistanceToNow } from 'date-fns';
+
+import styles from '../components/Post.module.css';
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import ptBR from 'date-fns/locale/pt-BR';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
+
+interface Author {
+    name: string; 
+    role: string;
+    avatarUrl: string;
+}
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string;
+    
+}
+
+interface postProps {
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+
+
+export function Post({author , publishedAt, content}:postProps) {
+    const [comments, setComments] = useState([
+        'Post muito massa'
+    ]);     
+
+    const [newCommentText, setNewCommentText] = useState('')
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
+        locale: ptBR,
+    })
+    /* format */
+    const pubblishedRelativeToNow = formatDistanceToNow (publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
+    function handleCreateNewComment(event: FormEvent){
+        event.preventDefault();
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentInvalid(event: InvalidEvent <HTMLTextAreaElement>){
+        event.target.setCustomValidity('Este campo é obrigatório')
+    }
+
+    function handleNewCommentChange (event: ChangeEvent<HTMLTextAreaElement>){
+        event.target.setCustomValidity('')
+        setNewCommentText(event.target.value)
+    }
+
+    function deleteComment(commentToDelete: string) {
+        const commentsWithoutDeleteOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+        setComments(commentsWithoutDeleteOne)
+    }
+    
+    const isNewCommentEmpty = newCommentText.length === 0
+
+    return (
+        <article className={styles.post}>
+            <header className={styles.headerTop}> 
+                <div className={styles.author}>
+                    <Avatar src={author.avatarUrl}/>
+                    <div className={styles.authorInfo}>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
+                    </div>
+                </div>
+
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()} >
+                    {pubblishedRelativeToNow}
+                </time> 
+            </header >
+
+            <div className={styles.content}>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p key={line.content}>{line.content}</p>
+                    }else if (line.type === 'link') {
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
+                    }
+                })}
+            </div>
+
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                
+                <textarea 
+                    name='comment'
+                    placeholder='Deixe um comentario'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
+                />
+
+                <footer>
+                    <button type='submit' disabled={isNewCommentEmpty}> Publicar </button>
+                </footer>
+            </form>
+            <div className={styles.commentList}>
+                {comments.map(comment => {
+                    return (
+                    <Comment 
+                    key={comment} 
+                    content = {comment} 
+                    onDeleteComment={deleteComment}
+                    />
+                    )
+                })}
+            </div>
+        </article>
+    )
+}
